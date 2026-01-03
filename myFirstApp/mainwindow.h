d71e03d2e3cfc66937caa5f99954997eb4bd0799
Den <dkaalinkov@yandex.ru>
2026-01-03 22:32:11 +0300

    1_add QT_project

---
+++ myFirstApp/mainwindow.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    double num1, num2, res;
+
+private slots:
+    void calculateAndShow();
+    void on_dial_1_valueChanged(int value);
+    void on_dial_2_valueChanged(int value);
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
